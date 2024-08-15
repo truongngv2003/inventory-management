554cb530e4ee3d8fc0cd9cefd51ed8ff7fe025ea
truongngv2003 <truongnguyenmailbka@gmail.com>
2024-08-15 22:41:29 +0700

    Add anonation Repo

--- src/main/java/com/rikkeisoft/inventory_management/repository/ManufacturerRepository.java
+++ src/main/java/com/rikkeisoft/inventory_management/repository/ManufacturerRepository.java
@@ -2,7 +2,9 @@ package com.rikkeisoft.inventory_management.repository;
 
 import com.rikkeisoft.inventory_management.model.Manufacturer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ManufacturerRepository extends JpaRepository<Manufacturer, Long> {
     boolean existsByName(String name);
 }
